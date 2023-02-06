896600d4c22a88db4631d6b29ac661a56632ee96
crisOlivares <cristhian@olivares.com>
2023-02-06 01:22:00 -0600

    initial commit

---
+++ src/main/java/com/example/demo/controladores/ControladorEjemplo.java
@@ -0,0 +1,25 @@
+package com.example.demo.controladores;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.ModelAndView;
+
+@RestController
+public class ControladorEjemplo {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(ControladorEjemplo.class);
+
+
+    @GetMapping("saludo")
+    public ModelAndView mostrarInicio(){
+        LOGGER.debug("mostrarInicio ()");
+        ModelAndView mav = new ModelAndView("bienvenida");
+        LOGGER.debug("bienvenida");
+        return mav;
+    }
+
+}
---
+++ src/main/webapp/META-INF/bienvenida.jsp
@@ -0,0 +1,8 @@
+<html>
+    <head>
+
+    </head>
+    <body>
+        <h3> Hola Mundo desde Spring framwork! </h3>
+    </body>
+</html>
