fc1fc8ab2a20b9b6741a1c78fa5bc045038a330e
crisOlivares <cristhian@olivares.com>
2023-02-08 00:00:32 -0600

    Busqueda por ID y eliminacion de registro implementada

--- src/main/java/com/example/demo/controladores/ControladorApicultores.java
+++ src/main/java/com/example/demo/controladores/ControladorApicultores.java
@@ -7,13 +7,11 @@ import com.example.demo.servicios.ServicioApicultores;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.net.URI;
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 public class ControladorApicultores {
@@ -34,6 +32,20 @@ public class ControladorApicultores {
         return this.repoApicultor.findAll();
     }
 
+    @GetMapping("apicultores/{idApicultor}")
+    public ResponseEntity<Apicultor> getPorId(
+            @PathVariable("idApicultor") String id
+    ){
+        LOGGER.debug("GET apicultores id:{}",id);
+        Optional<Apicultor> opa = this.repoApicultor.findById(id);
+        if(opa.isPresent()){
+            return ResponseEntity.ok(opa.get());
+        }
+        else{
+            return ResponseEntity.notFound().build();
+        }
+    }
+
     @PostMapping("apicultores")
     public ResponseEntity<Apicultor> crear(
             @RequestBody Apicultor nuevoA
@@ -49,11 +61,13 @@ public class ControladorApicultores {
                     .build();
         }
     }
-    /*
-    @GetMapping("apicultores/id")
-    public Apicultor getApicultorbyID(){
 
-        return this.repoApicultor.findBy();
+    @DeleteMapping("apicultores/{idApicultor}")
+    public ResponseEntity borrar(@PathVariable("idApicultor") String id){
+        this.repoApicultor.deleteById(id);
+        return ResponseEntity.ok().build();
     }
-     */
+
+
+
 }
